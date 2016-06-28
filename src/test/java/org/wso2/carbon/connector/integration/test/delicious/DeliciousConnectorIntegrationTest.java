335259e172eafb90ccad17f95e2ca12e933a4b92
RKathees <kathees@wso2.com>
2016-06-28 17:03:09 +0530

    Update DeliciousConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/delicious/DeliciousConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/delicious/DeliciousConnectorIntegrationTest.java
@@ -29,7 +29,7 @@ import javax.xml.namespace.QName;
 
 public class DeliciousConnectorIntegrationTest extends ConnectorIntegrationTestBase {
 
-    protected static final String CONNECTOR_NAME = "delicious-connector-1.0.1-SNAPSHOT";
+    protected static final String CONNECTOR_NAME = "delicious-connector-1.0.1";
     private String validAuthorization;
     private String invalidAuthorization;
 
